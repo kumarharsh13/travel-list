1f5db42e3d6bbc250ed75fc0ca5467c805217d81
kumarharsh13 <bigbangkumarharsh13@gmail.com>
2024-04-27 17:20:48 +0530

    Creating form and state management

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,10 +21,39 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState(""); // Use this controll the particular portion of the form
+  const [quantity, setQuantity] = useState(1); // Use this controll the particular portion of the form
+  function handleSubmit(e) {
+    e.preventDefault(); // Prevent page for performing default behaviour like page reload
+
+    if (!description) return;
+
+    const newItem = { description, quantity, package: false, id: Date.now() };
+    setDescription(""); // Seting it to the default state
+    setQuantity(1); // Seting it to the default state
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip ?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +62,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
